66798bfd07960d588095457d355654db8239e900
gjchow <gabrieljchow@gmail.com>
2021-09-16 15:45:44 -0400

    Added card buttons and toggling of form fields

--- mysite/quiz/static/scripts/details.js
+++ mysite/quiz/static/scripts/details.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
-    $(':input').removeClass('text-ans');
-    $(':input').addClass('text-disabled');
-    $(":input").prop('disabled', true);
+    $(':input[type=text]').removeClass('text-ans');
+    $(':input[type=text]').addClass('text-disabled');
+    $(':input[type=text]').prop('disabled', true);
+    $('#edit').click(function(){
+        $(':input[type=text]').toggleClass('text-ans');
+        $(':input[type=text]').toggleClass('text-disabled');
+        $(':input[type=text]').prop('disabled', function(i, v) { return !v; });
+        $('#edit').toggle();
+        $('#delete').toggle();
+        $('#save').toggle();
+        $('#cancel').toggle();
+    });
+    $('#save').click(function(){
+        $(':input[type=text]').toggleClass('text-ans');
+        $(':input[type=text]').toggleClass('text-disabled');
+        $(':input[type=text]').prop('disabled', function(i, v) { return !v; });
+        $('#edit').toggle();
+        $('#delete').toggle();
+        $('#save').toggle();
+        $('#cancel').toggle();
+    });
+    $('#cancel').click(function(){
+        $(':input[type=text]').toggleClass('text-ans');
+        $(':input[type=text]').toggleClass('text-disabled');
+        $(':input[type=text]').prop('disabled', function(i, v) { return !v; });
+        $('#edit').toggle();
+        $('#delete').toggle();
+        $('#save').toggle();
+        $('#cancel').toggle();
+    });
 });
